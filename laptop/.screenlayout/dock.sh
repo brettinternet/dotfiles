91d3266ed9a0a8036f8cd5aacfdc4a6ba0445c64
brett <brettinternet@gmail.com>
2021-06-16 15:03:21 -0600

    set dock xrandr primary display

--- laptop/.screenlayout/dock.sh
+++ laptop/.screenlayout/dock.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-xrandr --output DP-1.1 --mode 2560x1440 --pos 5120x0 --rotate normal --output DP-1.2 --mode 2560x1440 --pos 2560x0 --rotate normal --output DP-1.3 --mode 2560x1440 --pos 0x0 --rotate normal --output DP-0 --off --output DP-1 --off --output HDMI-0 --off --output eDP-1-1 --off
+xrandr --output DP-1.1 --mode 2560x1440 --pos 5120x0 --rotate normal --output DP-1.2 --primary --mode 2560x1440 --pos 2560x0 --rotate normal --output DP-1.3 --mode 2560x1440 --pos 0x0 --rotate normal --output DP-0 --off --output DP-1 --off --output HDMI-0 --off --output eDP-1-1 --off
