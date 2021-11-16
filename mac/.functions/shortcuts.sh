0babc186785ae7ec29bd2937f77bfd37eea24da8
brett <brettinternet@gmail.com>
2021-11-16 16:13:01 -0700

    add work folder alias

--- mac/.functions/shortcuts.sh
+++ mac/.functions/shortcuts.sh
@@ -9,6 +9,7 @@ PERSONAL_DIR="${MY_PROJECTS:-$DEV_DIR/me}"
 alias dev="cd $DEV_DIR"
 alias me="cd $PERSONAL_DIR"
 alias sandbox="cd $DEV_DIR/sandbox"
+alias work="cd $DEV_DIR/work"
 
 NOTES_DIR="${MY_NOTES:-$PERSONAL_DIR/notes}"
 # associated with a private folder with notes, delcared as `$MY_NOTES` in ~/.env
