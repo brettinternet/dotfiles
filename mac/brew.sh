8d01453fd7417ae15956d4e02754c570bf0be3a1
brett <brettinternet@gmail.com>
2022-12-29 12:11:56 -0700

    add brewfile

--- mac/brew.sh
+++ mac/brew.sh
@@ -1,63 +1,13 @@
 #!/bin/bash
 
-brew install \
-  git \
-  coreutils \
-  emacs \
-  neovim \
-  node \
-  elixir \
-  bpytop \
-  bat \
-  tmux \
-  broot \
-  asdf \
-  terminal-notifier \
-  nvm \
-  wget \
-  wireguard-tools \
-  qrencode \
-  virtualenvwrapper \
-  fd \
-  lsd \
-  bat \
-  # hidetatz/tap/kubecolor \ # broken: https://github.com/hidetatz/kubecolor/issues/90
-  trash-cli
-
-# Tiling?
-# https://github.com/koekeishiya/yabai/wiki/Installing-yabai-(latest-release)
-# koekeishiya/formulae/skhd
-# koekeishiya/formulae/yabai
-
-brew install --cask \
-  firefox \
-  eloston-chromium \
-  hyperdock \
-  bitwarden \
-  docker \
-  visual-studio-code \
-  iterm2 \
-  authy \
-  stats \
-  spotify \
-  dbeaver-community \
-  zoom \
-  cyberduck \
-  yippy \
-  homebrew/cask-fonts/font-fira-code \
-  gpg-suite \
-  karabiner-elements \
-  telegram \
-  home-assistant \
-  unnaturalscrollwheels \
-  font-hack-nerd-font
+brew bundle --file mac/Brewfile
 
 # Reset application attributes to allow run
 xattr -cr /Applications/Chromium.app
 xattr -cr /Applications/Yippy.app
 xattr -cr /Applications/UnnaturalScrollWheels.app
 
-xcode-select --install
+# xcode-select --install
 
 # vim plug
 if [[ ! -f ~/.vim/autoload/plug.vim ]]; then
