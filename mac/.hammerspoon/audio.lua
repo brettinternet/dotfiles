e9ab7fe76668b4f600be9726e58fd1cac08faacc
brett <brettinternet@gmail.com>
2024-03-26 15:09:48 -0600

    add mic mute menu bar indicator

--- mac/.hammerspoon/audio.lua
+++ mac/.hammerspoon/audio.lua
@@ -1,8 +1,30 @@
 -- A better push to talk / toggle mute
+local shouldCaffeinate = false
+local muteMenu = hs.menubar.new()
+
+local function setMuteDisplay(mute)
+  if mute then
+    muteMenu:setTitle("📵")
+  else
+    muteMenu:setTitle("🎤")
+  end
+end
+
 local function clearMuteAlert()
   if muteAlertId then
     hs.alert.closeSpecific(muteAlertId)
   end
+end
+
+local function setMute(mute)
+  local audio = hs.audiodevice.defaultInputDevice()
+  audio:setInputMuted(mute)
+  setMuteDisplay(mute)
+end
+
+local function getMuteState()
+  local audio = hs.audiodevice.defaultInputDevice()
+  return audio:inputMuted()
 end
 
 local holdingToTalk = false
@@ -13,26 +35,23 @@ local function pushToTalk()
   if muted then
     clearMuteAlert()
     muteAlertId = hs.alert.show("🎤 Microphone on", true)
-    audio:setInputMuted(false)
+    setMute(false)
   end
 end
 
 local function toggleMute()
-  local audio = hs.audiodevice.defaultInputDevice()
-  local muted = audio:inputMuted()
-  audio:setInputMuted(not muted)
+  setMute(not getMuteState())
 end
 
 local function toggleMuteOrPTT()
-  local audio = hs.audiodevice.defaultInputDevice()
-  local muted = audio:inputMuted()
+  local muted = getMuteState()
   local muting = not muted
   if holdingToTalk then
     holdingToTalk = false
-    audio:setInputMuted(true)
+    setMute(true)
     muting = true
   else
-    audio:setInputMuted(muting)
+    setMute(muting)
   end
   clearMuteAlert()
   if muting then
@@ -43,4 +62,10 @@ local function toggleMuteOrPTT()
 end
 
 hs.hotkey.bind({"cmd", "shift"}, "a", nil, toggleMuteOrPTT, pushToTalk)
-hs.hotkey.bind(nil, "f13", nil, toggleMute)
+hs.hotkey.bind(nil, "f18", nil, toggleMute)
+
+if muteMenu then
+  muteMenu:setClickCallback(toggleMute)
+  local muted = getMuteState()
+  setMuteDisplay(muted)
+end
