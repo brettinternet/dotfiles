797240580222215274e1fc47c149b2153e2ea77c
brett <brettinternet@gmail.com>
2024-11-11 16:18:31 -0700

    add homebrew install

--- darwin/setup.sh
+++ darwin/setup.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+homebrew_path="/opt/homebrew"
+if [ ! -d $homebrew_path ]; then
+  mkdir $homebrew_path
+  curl -L https://github.com/Homebrew/brew/tarball/master | tar xz --strip-components 1 -C $homebrew_path
+fi
+
 brew bundle --file darwin/Brewfile
 
 # Reset application attributes to allow run
