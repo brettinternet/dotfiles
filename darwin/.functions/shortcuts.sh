056baf3c4112480567fd76a516708dd6855aa937
brett <20871604+brettinternet@users.noreply.github.com>
2026-04-28 16:06:49 -0600

    remove unused alias shortcuts

--- darwin/.functions/shortcuts.sh
+++ darwin/.functions/shortcuts.sh
@@ -13,13 +13,6 @@ alias sandbox='cd $DEV_DIR/sandbox'
 alias work='cd $DEV_DIR/work'
 alias ai='cd $PERSONAL_DIR/ai'
 
-# shellcheck disable=SC2034
-NOTES_DIR="${MY_NOTES:-$PERSONAL_DIR/notes}"
-# associated with a private folder with notes, delcared as `$MY_NOTES` in ~/.envrc
-alias notes='cd $NOTES_DIR'
-alias todo='$EDITOR $NOTES_DIR/daily/todo'
-alias note='$EDITOR $NOTES_DIR/daily/notes'
-
 alias claude='claude --ide'
 
 function flush_dns() {
