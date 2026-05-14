ac764278f6a7d1bcc366b2992b892d5864b7cae0
brett <20871604+brettinternet@users.noreply.github.com>
2026-05-14 15:32:24 -0600

    tweak nvim conf

--- base/.config/nvim/lua/plugins/user.lua
+++ base/.config/nvim/lua/plugins/user.lua
@@ -1,18 +1,26 @@
 ---@type LazySpec
 return {
 
-  -- == Examples of Adding Plugins ==
+  -- Disable Aerial's symbols outline. AstroNvim v4 pins Aerial to v2, which is
+  -- not compatible with Neovim 0.12's Tree-sitter APIs, and this config does not
+  -- use the outline sidebar.
+  { "stevearc/aerial.nvim", enabled = false },
 
-  "andweeb/presence.nvim",
   {
-    "ray-x/lsp_signature.nvim",
-    event = "BufRead",
-    config = function() require("lsp_signature").setup() end,
+    "nvim-neo-tree/neo-tree.nvim",
+    opts = {
+      filesystem = {
+        filtered_items = {
+          visible = true,
+          hide_dotfiles = false,
+          hide_gitignored = false,
+        },
+      },
+    },
   },
 
-  -- == Examples of Overriding Plugins ==
-
-  -- customize alpha options
+  -- Keep a small personal dashboard customization and otherwise stay close to
+  -- AstroNvim defaults.
   {
     "goolord/alpha-nvim",
     opts = function(_, opts)
@@ -28,50 +36,6 @@ return {
         "                                                     ",
       }
       return opts
-    end,
-  },
-
-  -- You can disable default plugins as follows:
-  { "max397574/better-escape.nvim", enabled = false },
-
-  -- You can also easily customize additional setup of plugins that is outside of the plugin's setup call
-  {
-    "L3MON4D3/LuaSnip",
-    config = function(plugin, opts)
-      require "astronvim.plugins.configs.luasnip"(plugin, opts) -- include the default astronvim config that calls the setup call
-      -- add more custom luasnip configuration such as filetype extend or custom snippets
-      local luasnip = require "luasnip"
-      luasnip.filetype_extend("javascript", { "javascriptreact" })
-    end,
-  },
-
-  {
-    "windwp/nvim-autopairs",
-    config = function(plugin, opts)
-      require "astronvim.plugins.configs.nvim-autopairs"(plugin, opts) -- include the default astronvim config that calls the setup call
-      -- add more custom autopairs configuration such as custom rules
-      local npairs = require "nvim-autopairs"
-      local Rule = require "nvim-autopairs.rule"
-      local cond = require "nvim-autopairs.conds"
-      npairs.add_rules(
-        {
-          Rule("$", "$", { "tex", "latex" })
-            -- don't add a pair if the next character is %
-            :with_pair(cond.not_after_regex "%%")
-            -- don't add a pair if  the previous character is xxx
-            :with_pair(
-              cond.not_before_regex("xxx", 3)
-            )
-            -- don't move right when repeat character
-            :with_move(cond.none())
-            -- don't delete if the next character is xx
-            :with_del(cond.not_after_regex "xx")
-            -- disable adding a newline when you press <cr>
-            :with_cr(cond.none()),
-        },
-        -- disable for .vim files, but it work for another filetypes
-        Rule("a", "a", "-vim")
-      )
     end,
   },
 }
